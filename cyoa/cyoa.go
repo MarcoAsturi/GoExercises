6831f846e2f2a4441c66a21139b6db940a2b5f85
MarcoAsturi <marco.asturi@gmail.com>
2023-04-17 12:34:01 +0200

    rewritten code and fixed template.html

--- cyoa/cyoa.go
+++ cyoa/cyoa.go
@@ -2,142 +2,84 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"html/template"
 	"log"
 	"net/http"
-	"text/template"
+	"os"
+	"strings"
 )
 
-// define a struct for story arcs
-type storyArc struct {
+// Chapter rappresents a single chapter of the story
+type Chapter struct {
 	Title   string   `json:"title"`
 	Story   []string `json:"story"`
-	Options []struct {
-		Text string `json:"text"`
-		Arc  string `json:"arc"`
-	} `json:"options"`
+	Options []Option `json:"options"`
 }
 
-func parseStory(filename string) (map[string]storyArc, error) {
-	// TODO: read json file and return a map
-	// the map contain title arc as key
-	content, err := ioutil.ReadFile(filename)
+type Option struct {
+	Text    string `json:"text"`
+	Chapter string `json:"arc"`
+}
+
+// Story represents the entire story
+// it is a map: the key is the title chapter and the value is the chapter
+type Story map[string]Chapter
+
+// load the json file
+func loadStory(filename string) (Story, error) {
+	// open file inb reading mode
+	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
 	}
+	defer file.Close() // REMEMBER to close it
 
-	var story []storyArc
-	if err := json.Unmarshal(content, &story); err != nil {
+	// decoding the json content in a Chapter map
+	var story Story
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&story); err != nil { // dont FORGET pointers
 		return nil, err
 	}
-
-	storyMap := make(map[string]storyArc)
-
-	for _, arc := range story {
-		storyMap[arc.Title] = arc
-	}
-
-	return storyMap, nil
+	return story, nil
 }
 
-func executeTemplate(w http.ResponseWriter, tmpl *template.Template, story storyArc) {
-	// TODO: execute html and send to the client
-	err := tmpl.Execute(w, story)
-	if err != nil {
-		log.Fatal("could not execute template")
+// handle HTTP request for a specific chapter
+func chapterHandler(story Story) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// remove space and "/"  from HTTP request
+		path := strings.TrimSpace(r.URL.Path)
+		if path == "" || path == "/" {
+			path = "/intro"
+		} else {
+			path = path[1:]
+		}
+
+		// if the request chapter is present in the map, return it using html template
+		if chapter, ok := story[path]; ok {
+			t := template.Must(template.ParseFiles("template.html")) // USE .Must
+			if err := t.Execute(w, chapter); err != nil {
+				log.Printf("template error: %v", err)
+				http.Error(w, "internal server error", http.StatusInternalServerError)
+			}
+		} else {
+			http.NotFound(w, r)
+		}
 	}
-}
-
-// manage the starter request and display the first arc
-func startHandler(w http.ResponseWriter, r *http.Request, storyMap map[string]storyArc) {
-	story := storyMap["intro"]
-	tmpl, err := template.New("").Parse(defaultHandlerTmpl)
-	if err != nil {
-		log.Fatal("could not parse template")
-	}
-	executeTemplate(w, tmpl, story)
-}
-
-// manage the requestes for netx arcs and display the corresponding arc
-func storyHandler() {
-
 }
 
 func main() {
-	storyMap, err := parseStory("gopher.json")
+	// load story
+	story, err := loadStory("gopher.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// http.HandleFunc("/", startHandler)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		startHandler(w, r, storyMap)
-	})
-	http.ListenAndServe(":8080", nil)
-}
+	// setting up the handler
+	http.HandleFunc("/", chapterHandler(story))
 
-var defaultHandlerTmpl = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Choose Your Own Adventure</title>
-  </head>
-  <body>
-    <section class="page">
-      <h1>{{.Title}}</h1>
-      {{range .Paragraphs}}
-        <p>{{.}}</p>
-      {{end}}
-      {{if .Options}}
-        <ul>
-        {{range .Options}}
-          <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
-        {{end}}
-        </ul>
-      {{else}}
-        <h3>The End</h3>
-      {{end}}
-    </section>
-    <style>
-      body {
-        font-family: helvetica, arial;
-      }
-      h1 {
-        text-align:center;
-        position:relative;
-      }
-      .page {
-        width: 80%;
-        max-width: 500px;
-        margin: auto;
-        margin-top: 40px;
-        margin-bottom: 40px;
-        padding: 80px;
-        background: #FFFCF6;
-        border: 1px solid #eee;
-        box-shadow: 0 10px 6px -6px #777;
-      }
-      ul {
-        border-top: 1px dotted #ccc;
-        padding: 10px 0 0 0;
-        -webkit-padding-start: 0;
-      }
-      li {
-        padding-top: 10px;
-      }
-      a,
-      a:visited {
-        text-decoration: none;
-        color: #6295b5;
-      }
-      a:active,
-      a:hover {
-        color: #7792a2;
-      }
-      p {
-        text-indent: 1em;
-      }
-    </style>
-  </body>
-</html>`
+	port := "8080"
+	log.Printf("listening on http://localhost:%s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
